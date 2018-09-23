646469de041fdca751fe8e618e1ce5cce69a4cdb
Ivo Georgiev <ivo@adex.network>
2018-09-23 23:34:25 +0300

    SafeERC20 added

---
+++ contracts/libs/SafeERC20.sol
@@ -0,0 +1,53 @@
+pragma solidity 0.4.24;
+
+import "../interfaces/ERC20.sol";
+
+library SafeERC20 {
+	// definitely not a pure fn but the compiler complains otherwise
+	function checkSuccess()
+		private
+		pure
+		returns (bool)
+	{
+		uint256 returnValue = 0;
+
+		assembly {
+			// check number of bytes returned from last function call
+			switch returndatasize
+
+			// no bytes returned: assume success
+			case 0x0 {
+				returnValue := 1
+			}
+
+			// 32 bytes returned: check if non-zero
+			case 0x20 {
+				// copy 32 bytes into scratch space
+				returndatacopy(0x0, 0x0, 0x20)
+
+				// load those bytes into returnValue
+				returnValue := mload(0x0)
+			}
+
+			// not sure what was returned: don't mark as success
+			default { }
+		}
+
+		return returnValue != 0;
+	}
+
+	function transfer(address token, address to, uint256 amount) internal {
+		ERC20(token).transfer(to, amount);
+		require(checkSuccess());
+	}
+
+	function transferFrom(address token, address from, address to, uint256 amount) internal {
+ 		ERC20(token).transferFrom(from, to, amount);
+		require(checkSuccess());
+	}
+
+	function approve(address token, address spender, uint256 amount) internal {
+ 		ERC20(token).approve(spender, amount);
+ 		require(checkSuccess());
+	}
+}
