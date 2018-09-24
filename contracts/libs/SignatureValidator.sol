18b29dafcc51b74da05c86b58f40a18fdf3fe2f8
Ivo Georgiev <ivo@strem.io>
2018-09-24 10:47:06 +0300

    Avoid wasting space with byte[66]; also removes assembly in SignatureValidator

--- contracts/libs/SignatureValidator.sol
+++ contracts/libs/SignatureValidator.sol
@@ -11,22 +11,16 @@ library SignatureValidator {
 	/// @dev Validates that a hash was signed by a specified signer.
 	/// @param hash Hash which was signed.
 	/// @param signer Address of the signer.
-	/// @param signature ECDSA signature along with the mode (0 = EIP712, 1 = Geth, 2 = Trezor) {mode}{v}{r}{s}.
+	/// @param signature ECDSA signature along with the mode (0 = EIP712, 1 = Geth, 2 = Trezor) [{mode}{v}, {r}, {s}]
 	/// @return Returns whether signature is from a specified user.
-	function isValidSignature(bytes32 hash, address signer, byte[66] signature) internal pure returns (bool) {
-		SignatureMode mode = SignatureMode(uint8(signature[0]));
+	function isValidSignature(bytes32 hash, address signer, bytes32[3] signature) internal pure returns (bool) {
+		SignatureMode mode = SignatureMode(uint8(signature[0][0]));
 
 		if (mode == SignatureMode.NO_SIG) {
 			return false;
 		}
 
-		uint8 v = uint8(signature[1]);
-		bytes32 r;
-		bytes32 s;
-		assembly {
-			r := mload(add(signature, 34))
-			s := mload(add(signature, 66))
-		}
+		uint8 v = uint8(signature[0][1]);
 
 		if (mode == SignatureMode.GETH) {
 			hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
@@ -34,6 +28,6 @@ library SignatureValidator {
 			hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n\x20", hash));
 		}
 
-		return ecrecover(hash, v, r, s) == signer;
+		return ecrecover(hash, v, signature[1], signature[2]) == signer;
 	}
 }
