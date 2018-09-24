f132d45a1b9d9c5b0fc5579a8002a1bc0e7b8806
Ivo Georgiev <ivo@strem.io>
2018-09-24 18:38:36 +0300

    test/TestSafeERC20 added

---
+++ test/mocks/BadToken.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.24;
+
+import "../../contracts/libs/SafeMath.sol";
+
+contract BadToken {
+	mapping (address => uint) balances;
+
+	function balanceOf(address owner) public view returns (uint) {
+		return balances[owner];
+	}
+
+	// Breaks ERC20 spec: those 2 should return (bool)
+	function transfer(address to, uint value) public {
+		balances[msg.sender] = SafeMath.sub(balances[msg.sender], value);
+		balances[to] = SafeMath.add(balances[to], value);
+	}
+
+	function transferFrom(address from, address to, uint value) public {
+		balances[from] = SafeMath.sub(balances[from], value);
+		balances[to] = SafeMath.add(balances[to], value);
+	}
+
+	function setBalanceTo(address to, uint value) public {
+		balances[to] = value;
+	}
+}
---
+++ test/mocks/WorstToken.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.4.24;
+
+contract WorstToken {
+	mapping (address => uint) balances;
+
+	function balanceOf(address owner) public view returns (uint) {
+		return balances[owner];
+	}
+
+	function transfer(address to, uint value) public returns (bool) {
+		if (value > balances[msg.sender]) {
+			return false;
+		}
+		balances[msg.sender] = balances[msg.sender] - value;
+		balances[to] = balances[to] + value;
+		return true;
+	}
+
+	function transferFrom(address from, address to, uint value) public returns (bool) {
+		if (value > balances[from]) {
+			return false;
+		}
+		balances[from] = balances[from] - value;
+		balances[to] = balances[to] + value;
+		return true;
+	}
+
+	function setBalanceTo(address to, uint value) public {
+		balances[to] = value;
+	}
+}
