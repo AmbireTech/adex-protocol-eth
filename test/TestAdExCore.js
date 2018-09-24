f41596afbc3938d6cc3136a89f9f92116d5a032c
Ivo Georgiev <ivo@adex.network>
2018-09-24 23:56:39 +0300

    WIP js lib for bid

--- test/TestAdExCore.js
+++ test/TestAdExCore.js
@@ -6,7 +6,6 @@ const MockToken = artifacts.require('./mocks/Token')
 // @TODO: use eth-sig-util in the tests, so we can conform with what metamask does
 // https://github.com/MetaMask/eth-sig-util/blob/master/index.js
 // https://github.com/ethereumjs/ethereumjs-abi/blob/master/lib/index.js
-const bidSchemaHash = '0xf05a6d38810408971c1e2a9cd015fefd95aaae6d0c1a25da4ed10c1ac77ebb64'
 const commitmentSchemaHash = '0x8aa1fb0e671ad6f7d73ad552eff29b7b79186e0143b91e48a013151a34ae50dd'
 
 contract('AdExCore', function(accounts) {
