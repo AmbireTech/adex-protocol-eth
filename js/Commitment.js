36cc8dd2528f9311c9ff31e857f3fdcef8b08e15
Ivo Georgiev <ivo@adex.network>
2018-09-25 23:39:35 +0300

    JS libs: hash finished (WARNING: does not match with sol yet)

--- js/Commitment.js
+++ js/Commitment.js
@@ -5,12 +5,12 @@ const ethereumAbi = require('ethereumjs-abi')
 // https://github.com/MetaMask/eth-sig-util/blob/master/index.js
 // https://github.com/ethereumjs/ethereumjs-abi/blob/master/lib/index.js
 const abi = require('ethereumjs-abi')
+const keccak256 = require('js-sha3').keccak256
 const ensure = require('./ensureTypes')
 
 const SCHEMA_HASH = '0x8aa1fb0e671ad6f7d73ad552eff29b7b79186e0143b91e48a013151a34ae50dd'
 
 function Commitment(args) {
-	console.log(args)
 	this.bidId = ensure.Bytes32(args.bidId)
 	
 	this.tokenAddr = ensure.Address(args.tokenAddr)
@@ -31,11 +31,15 @@ function Commitment(args) {
 	return this
 }
 
+Commitment.prototype.values = function() {
+	return [this.bidId, this.tokenAddr, this.tokenAmount, this.validUntil, this.advertiser, this.publisher]
+}
+
 Commitment.prototype.hash = function() {
-	return abi.soliditySHA3(
-		['bytes32', 'bytes32', 'address', 'uint256', 'uint256', 'address[]', 'uint256[]'],
-		[SCHEMA_HASH, this.bidId, this.tokenAddr, this.tokenAmount, this.nonce, this.validators, this.validatorRewards]
-	)
+	return keccak256(abi.rawEncode(
+		['bytes32', 'bytes32', 'address', 'uint256', 'address', 'address', 'address[]', 'uint256[]'],
+		[SCHEMA_HASH, this.bidId, this.tokenAddr, this.tokenAmount, this.advertiser, this.publisher, this.validators, this.validatorRewards]
+	))
 }
 
 module.exports = { Commitment, SCHEMA_HASH }
