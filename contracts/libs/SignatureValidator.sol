37a4741fef2e373ea71ccb22915ef97e5664ba72
Ivo Georgiev <ivo@strem.io>
2018-10-01 10:19:37 +0300

    contracts/libs/SignatureValidator: add AdEx prefix mode

--- contracts/libs/SignatureValidator.sol
+++ contracts/libs/SignatureValidator.sol
@@ -5,13 +5,14 @@ library SignatureValidator {
 		NO_SIG,
 		EIP712,
 		GETH,
-		TREZOR
+		TREZOR,
+		ADEX
 	}
 
 	/// @dev Validates that a hash was signed by a specified signer.
 	/// @param hash Hash which was signed.
 	/// @param signer Address of the signer.
-	/// @param signature ECDSA signature along with the mode (0 = EIP712, 1 = Geth, 2 = Trezor) [{mode}{v}, {r}, {s}]
+	/// @param signature ECDSA signature along with the mode [{mode}{v}, {r}, {s}]
 	/// @return Returns whether signature is from a specified user.
 	function isValidSignature(bytes32 hash, address signer, bytes32[3] signature) internal pure returns (bool) {
 		SignatureMode mode = SignatureMode(uint8(signature[0][0]));
@@ -26,6 +27,8 @@ library SignatureValidator {
 			hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
 		} else if (mode == SignatureMode.TREZOR) {
 			hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n\x20", hash));
+		} else if (mode == SignatureMode.ADEX) {
+			hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n99By signing this message, you acknowledge signing an AdEx bid with the hash:\n"));
 		}
 
 		return ecrecover(hash, v, signature[1], signature[2]) == signer;
