70f68b7ce7b75a8f153e1349de707c014ebfb88a
Ivo Georgiev <ivo@adex.network>
2018-10-30 14:56:01 +0100

    MerkleProof lib

---
+++ contracts/libs/MerkleProof.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.4.24;
+
+library MerkleProof {
+	function isContained(bytes32 valueHash, bytes32[] proof, bytes32 root) internal pure returns (bool) {
+		bytes32 cursor = valueHash;
+
+		for (uint256 i = 0; i!=proof.length; i++) {
+			if (cursor < proof[i]) {
+				cursor = keccak256(abi.encodePacked(cursor, proof[i]));
+			} else {
+				cursor = keccak256(abi.encodePacked(proof[i], cursor));
+			}
+		}
+
+		return cursor == root;
+	}
+}
+
