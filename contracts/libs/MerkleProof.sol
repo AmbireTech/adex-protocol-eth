157260d294df9e8006a7f4eb9c1b9d3984cd1767
Ivo Georgiev <ivo@adex.network>
2018-10-31 09:46:53 +0100

    MerkleProof: something to be considered

--- contracts/libs/MerkleProof.sol
+++ contracts/libs/MerkleProof.sol
@@ -4,6 +4,7 @@ library MerkleProof {
 	function isContained(bytes32 valueHash, bytes32[] proof, bytes32 root) internal pure returns (bool) {
 		bytes32 cursor = valueHash;
 
+		// NOTE: Should we end up in a situation where we compare valueHash == root if the proof is empty?
 		for (uint256 i = 0; i!=proof.length; i++) {
 			if (cursor < proof[i]) {
 				cursor = keccak256(abi.encodePacked(cursor, proof[i]));
