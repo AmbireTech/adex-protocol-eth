7684ee11872fcea779e5b3ea26a755157c0916bc
Ivo Georgiev <ivo@strem.io>
2018-11-05 03:04:17 +0200

    TestAdExCore: test ChannelWithdrawExpired

--- test/TestAdExCore.js
+++ test/TestAdExCore.js
@@ -2,13 +2,14 @@ const AdExCore = artifacts.require('AdExCore')
 const MockToken = artifacts.require('./mocks/Token')
 const MockLibs = artifacts.require('./mocks/Libs')
 
-//const { Bid, BidState } = require('../js/Bid')
-//const Commitment = require('../js/Commitment').Commitment
 const splitSig = require('../js/splitSig')
 
 const promisify = require('util').promisify
 const ethSign = promisify(web3.eth.sign.bind(web3))
 
+// @TODO test some stuff, e.g. SignatureValidator, via the built-in web3; do not require ethers at all here, but require it in the Channel js lib
+//const { Bid, BidState } = require('../js/Bid')
+//const Commitment = require('../js/Commitment').Commitment
 const { providers, Contract } = require('ethers');
 const web3Provider = new providers.Web3Provider(web3.currentProvider)
 
@@ -31,18 +32,44 @@ contract('AdExCore', function(accounts) {
 	it('channelOpen', async function() {
 		const tokens = 2000
 		await token.setBalanceTo(accounts[0], tokens)
-		const tx = await core.channelOpen([accounts[0], token.address, tokens, 1543622400, [accounts[0],accounts[1]],"0x0202020202020202020202020202020202020202020202020202020202020202"])
+		const channel = [accounts[0], token.address, tokens, Math.floor(Date.now()/1000)+50, [accounts[0], accounts[1]], '0x0202020202020202020202020202020202020202020202020202020202020202']
+		const tx = await core.channelOpen(channel)
 		const receipt = await tx.wait()
 		assert.ok(receipt.events.find(x => x.event === 'LogChannelOpen'), 'has LogChannelOpen event')
 
 		assert.equal(await token.balanceOf(accounts[0]), 0, 'account balance is 0')
 		assert.equal(await token.balanceOf(core.address), tokens, 'contract balance is correct')
+
+		// @TODO state has updated
 	})
 
-	/*it('channelExpiredWithdraw', async function() {
+	// @TODO hash match between this channel and the JS lib
+	// @TODO: SignatureValidator test via the mock lib
+
+	it('channelWithdrawExpired', async function() {
+		const tokens = 2000
+		await token.setBalanceTo(accounts[0], tokens)
+		// @TODO: getSampleChannel or some helper? the js lib?
+		const channel = [accounts[0], token.address, tokens, Math.floor(Date.now()/1000)+50, [accounts[0], accounts[1]], '0x0202020202020202020202020202020202020202020202020202020202020203']
+		await (await core.channelOpen(channel)).wait()
+		try {
+			await (await core.channelWithdrawExpired(channel))
+			assert.isOk(false, 'channelWithdrawExpired succeeded too early')
+		} catch(e) {
+			assert.isOk(e.message.match(/VM Exception while processing transaction: revert NOT_EXPIRED/), 'cannot timeout that early')
+		}
+
+		await moveTime(web3, 100)
+
+		const receipt = await (await core.channelWithdrawExpired(channel)).wait()
+		assert.ok(receipt.events.find(x => x.event === 'LogChannelWithdrawExpired'), 'has LogChannelWihtdrawExpired event')
+		// @TODO ensure can't withdraw after it's expired; maybe verify that we can BEFORE via gas estimations
+		// @TODO check balances, etc.
 	})
 
+	/*
 	it('channelWithdraw', async function() {
+		// @TODO completely exhaust channel, use getWithdrawn to ensure it's exhausted (or have a JS lib convenience method)
 	})
 	*/
 
