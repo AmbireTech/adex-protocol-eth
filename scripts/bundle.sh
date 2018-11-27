b9df617829661a7518ee10f4cb6c4108659dd6d5
Ivo Georgiev <ivo@strem.io>
2018-11-27 22:40:24 +0200

    README: add verifying on etherscan

---
+++ scripts/bundle.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash 
+
+# simple version, does not work because of no dedup
+#echo $1 >&2
+#cat $1 | egrep "^import " | cut -d"\"" -f2 | cut -d"'" -f2 | while read line; do $0 "$(dirname $1)/$line"; done
+#cat $1 | egrep -v "^(pragma solidity |import )"
+
+
+listFiles() {
+	# macOS users: brew install coreutils
+	cat $1 | egrep "^import " | cut -d"\"" -f2 | cut -d"'" -f2 | while read line; do listFiles $(dirname $1)/$line; done
+        realpath $1
+}
+
+egrep -h "^pragma solidity" $1 | head -n1
+
+listFiles $1 | awk '!x[$0]++' | while read line; do
+	cat $line | egrep -v "^(pragma solidity |import )" 
+done
+
