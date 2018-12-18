46a8e48ec2817e1b0f6b333a0f075d8a318e6761
Ivo Georgiev <ivo@strem.io>
2018-12-18 10:15:26 +0200

    contracts/libs/MerkleProof: gas saving in the loop condition, removed confusing comment

--- contracts/libs/MerkleProof.sol
+++ contracts/libs/MerkleProof.sol
@@ -4,8 +4,7 @@ library MerkleProof {
 	function isContained(bytes32 valueHash, bytes32[] memory proof, bytes32 root) internal pure returns (bool) {
 		bytes32 cursor = valueHash;
 
-		// NOTE: Should we end up in a situation where we compare valueHash == root if the proof is empty?
-		for (uint256 i = 0; i!=proof.length; i++) {
+		for (uint256 i = 0; i < proof.length; i++) {
 			if (cursor < proof[i]) {
 				cursor = keccak256(abi.encodePacked(cursor, proof[i]));
 			} else {
