4aa3ba3c42ab9c68ce86119c4948645866493cfb
Ivo Georgiev <ivo@adex.network>
2018-12-25 19:39:53 +0200

    SafeERC20: add balanceOf

--- contracts/libs/SafeERC20.sol
+++ contracts/libs/SafeERC20.sol
@@ -4,6 +4,7 @@ interface GeneralERC20 {
 	function transfer(address to, uint256 value) external;
 	function transferFrom(address from, address to, uint256 value) external;
 	function approve(address spender, uint256 value) external;
+	function balanceOf(address spender) external view returns (uint);
 }
 
 library SafeERC20 {
