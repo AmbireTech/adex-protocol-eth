557fc4aa034925d635c0618a9846b7346cf198e6
Ivo Georgiev <ivo@strem.io>
2018-12-25 02:30:39 +0200

    Identity: routine calls done

---
+++ contracts/interfaces/AdExCoreInterface.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.25;
+pragma experimental ABIEncoderV2;
+
+import "../libs/ChannelLibrary.sol";
+
+contract AdExCoreInterface {
+	function channelOpen(ChannelLibrary.Channel memory channel) public;
+	function channelWithdrawExpired(ChannelLibrary.Channel memory channel) public;
+	function channelWithdraw(ChannelLibrary.Channel memory channel, bytes32 stateRoot, bytes32[3][] memory signatures, bytes32[] memory proof, uint amountInTree) public;
+}
