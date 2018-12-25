04cb9abdc59506e1d903ae765ca6b422d45986f5
Ivo Georgiev <ivo@strem.io>
2018-12-25 16:40:32 +0200

    Identity: explain problem when using abi.encode

--- contracts/extra/Identity.sol
+++ contracts/extra/Identity.sol
@@ -76,7 +76,7 @@ contract Identity {
 		for (uint i=0; i<transactions.length; i++) {
 			Transaction memory transaction = transactions[i];
 			//bytes32 hash = keccak256(abi.encode(transaction));
-			// @TODO: riperoni, fix this
+			// @TODO: riperoni, fix this; without `bytes`-typed fields, it's the same
 			bytes32 hash = keccak256(abi.encode(transaction.identityContract, transaction.nonce, transaction.feeTokenAddr, transaction.feeTokenAmount, transaction.to, transaction.data));
 			address signer = SignatureValidator.recoverAddr(hash, signatures[i]);
 
