f13d6e637bbc81298de67aba1fc539d64377a69c
Ivo Georgiev <ivo@adex.network>
2018-12-25 19:14:57 +0200

    mock token now emits Transfer

--- test/mocks/Token.sol
+++ test/mocks/Token.sol
@@ -4,7 +4,7 @@ import "../../contracts/libs/SafeMath.sol";
 
 contract Token {
 	mapping (address => uint) balances;
-
+	event Transfer(address indexed from, address indexed to, uint value);
 	function balanceOf(address owner) public view returns (uint) {
 		return balances[owner];
 	}
@@ -12,12 +12,14 @@ contract Token {
 	function transfer(address to, uint value) public returns (bool) {
 		balances[msg.sender] = SafeMath.sub(balances[msg.sender], value);
 		balances[to] = SafeMath.add(balances[to], value);
+		emit Transfer(msg.sender, to, value);
 		return true;
 	}
 
 	function transferFrom(address from, address to, uint value) public returns (bool) {
 		balances[from] = SafeMath.sub(balances[from], value);
 		balances[to] = SafeMath.add(balances[to], value);
+		emit Transfer(from, to, value);
 		return true;
 	}
 
