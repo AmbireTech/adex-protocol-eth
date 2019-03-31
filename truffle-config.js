b4b9732ae2a6d738d756fa8202c070c3e3c3d1a3
Ivo Georgiev <ivo@strem.io>
2019-03-31 23:28:38 +0300

    truffle-config updated

--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,14 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
+    //development: {
+    //  host: 'localhost',
+    //  port: 8545,
+    //  network_id: '*', // Match any network id
+    //  gas: 4500000,
+    //  gasPrice: 10000000000,
+    //},
+	  // development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
@@ -91,13 +98,13 @@ module.exports = {
     solc: {
        version: "0.5.6",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
       //    enabled: false,
       //    runs: 200
       //  },
-      //  evmVersion: "byzantium"
-      // }
+        evmVersion: "constantinople"
+       }
     }
   }
 }
