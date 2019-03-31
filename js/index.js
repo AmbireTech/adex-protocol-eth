a38b50c05da5021e57db4cb4afb12a3315be304f
Ivo Georgiev <ivo@strem.io>
2019-03-31 23:44:33 +0300

    Identity: drop previous way to get counterfactual deploy tx, use CREATE2 only

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 const channelLib = require('./Channel')
 const identityLib = require('./Identity')
-const identityDeployLib = require('./IdentityDeploy')
 module.exports = {
 	...channelLib,
 	...identityLib,
-	...identityDeployLib,
 	MerkleTree: require('./MerkleTree'),
 	splitSig: require('./splitSig'),
 }
