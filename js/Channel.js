4cd25fee0c260a8bd938fb4eb904c5daae115116
Ivo Georgiev <ivo@adex.network>
2019-04-03 01:48:48 +0300

    js/Channel: eslint, use Buffer.from, proper errors

--- js/Channel.js
+++ js/Channel.js
@@ -5,7 +5,7 @@ const ensure = require('./ensureTypes')
 const ChannelState = {
 	Unknown: 0,
 	Active: 1,
-	Expired: 2,
+	Expired: 2
 }
 
 function Channel(args) {
@@ -28,22 +28,39 @@ function Channel(args) {
 
 Channel.prototype.hash = function(contractAddr) {
 	// contains contractAddr, so that it's not replayable
-	if (!contractAddr) throw 'contractAddr required'
-	return new Buffer(keccak256.arrayBuffer(
-		abi.rawEncode(
-			['address', 'address', 'address', 'uint256', 'uint256', 'address[]', 'bytes32'],
-			[contractAddr, this.creator, this.tokenAddr, this.tokenAmount, this.validUntil, this.validators, this.spec]
+	if (!contractAddr) throw new Error('contractAddr required')
+	return Buffer.from(
+		keccak256.arrayBuffer(
+			abi.rawEncode(
+				['address', 'address', 'address', 'uint256', 'uint256', 'address[]', 'bytes32'],
+				[
+					contractAddr,
+					this.creator,
+					this.tokenAddr,
+					this.tokenAmount,
+					this.validUntil,
+					this.validators,
+					this.spec
+				]
+			)
 		)
-	))
+	)
 }
 
 Channel.prototype.hashHex = function(contractAddr) {
-	return '0x'+this.hash(contractAddr).toString('hex')
+	return `0x${this.hash(contractAddr).toString('hex')}`
 }
 
 Channel.prototype.toSolidityTuple = function() {
 	// etherjs doesn't seem to want BN.js instances; hex is the lowest common denominator for web3/ethers
-	return [this.creator, this.tokenAddr, '0x'+this.tokenAmount.toString(16), '0x'+this.validUntil.toString(16), this.validators, this.spec]
+	return [
+		this.creator,
+		this.tokenAddr,
+		`0x${this.tokenAmount.toString(16)}`,
+		`0x${this.validUntil.toString(16)}`,
+		this.validators,
+		this.spec
+	]
 }
 
 Channel.prototype.hashToSign = function(contractAddr, balanceRoot) {
@@ -52,22 +69,18 @@ Channel.prototype.hashToSign = function(contractAddr, balanceRoot) {
 }
 
 Channel.prototype.hashToSignHex = function(contractAddr, stateRoot) {
-	return '0x'+this.hashToSign(contractAddr, stateRoot).toString('hex')
+	return `0x${this.hashToSign(contractAddr, stateRoot).toString('hex')}`
 }
 
 // This returns the same as .hashToSign, .hashToSignHex, but it takes the channelId rather than (the whole channel + contract addr)
 Channel.getSignableStateRoot = function(channelId, balanceRoot) {
 	return Buffer.from(
-		keccak256.arrayBuffer(
-			abi.rawEncode(['bytes32', 'bytes32'], [ channelId, balanceRoot ])
-		)
+		keccak256.arrayBuffer(abi.rawEncode(['bytes32', 'bytes32'], [channelId, balanceRoot]))
 	)
 }
 
 Channel.getBalanceLeaf = function(acc, amnt) {
-	return new Buffer(keccak256.arrayBuffer(
-		abi.rawEncode(['address', 'uint256'], [acc, amnt])
-	))
+	return Buffer.from(keccak256.arrayBuffer(abi.rawEncode(['address', 'uint256'], [acc, amnt])))
 }
 
 module.exports = { Channel, ChannelState }
