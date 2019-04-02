ee1465cb2cb8dbcdfb4ad698f461110a50911198
Ivo Georgiev <ivo@strem.io>
2019-04-02 18:13:15 +0300

    IdentityDeploy: fix formatting

--- contracts/extra/IdentityFactory.sol
+++ contracts/extra/IdentityFactory.sol
@@ -1,18 +1,18 @@
 pragma solidity ^0.5.6;
 
 contract IdentityFactory {
-  event Deployed(address addr, uint256 salt);
+	event Deployed(address addr, uint256 salt);
 
-  function deploy(bytes memory code, uint256 salt) public {
-    address addr;
-    assembly {
-      addr := create2(0, add(code, 0x20), mload(code), salt)
-    }
-    require(
-      addr != address(0),
-      "FAILED_DEPLOYING"
-    );
+	function deploy(bytes memory code, uint256 salt) public {
+		address addr;
+		assembly {
+			addr := create2(0, add(code, 0x20), mload(code), salt)
+		}
+		require(
+			addr != address(0),
+			"FAILED_DEPLOYING"
+		);
 
-    emit Deployed(addr, salt);
-  }
+		emit Deployed(addr, salt);
+	}
 }
