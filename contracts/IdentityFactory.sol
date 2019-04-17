06aae0ae8f9e797452d51faf37e7215a1460bda5
Ivo Georgiev <ivo@adex.network>
2019-04-17 15:18:40 +0300

    deployAndExecute added

--- contracts/IdentityFactory.sol
+++ contracts/IdentityFactory.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.6;
+pragma experimental ABIEncoderV2;
 
 import "./libs/SafeERC20.sol";
+import "./Identity.sol";
 
 contract IdentityFactory {
 	event LogDeployed(address addr, uint256 salt);
@@ -23,6 +25,14 @@ contract IdentityFactory {
 		assembly { addr := create2(0, add(code, 0x20), mload(code), salt) }
 		require(addr != address(0), "FAILED_DEPLOYING");
 		SafeERC20.transfer(tokenAddr, addr, tokenAmount);
+		emit LogDeployed(addr, salt);
+	}
+
+	function deployAndExecute(bytes memory code, uint256 salt, Identity.Transaction[] memory txns, bytes32[3][] memory signatures) public {
+		address addr;
+		assembly { addr := create2(0, add(code, 0x20), mload(code), salt) }
+		require(addr != address(0), "FAILED_DEPLOYING");
+		Identity(addr).execute(txns, signatures);
 		emit LogDeployed(addr, salt);
 	}
 }
