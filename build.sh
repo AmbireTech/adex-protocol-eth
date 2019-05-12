c2df5b358023b39e879b0c990626351b283ae060
Ivo Georgiev <ivo@adex.network>
2019-05-12 21:28:42 +0300

    add abi/Registry

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ mkdir -p temp
 solc --abi --bin contracts/AdExCore.sol -o temp 
 solc --overwrite --abi contracts/Identity.sol -o temp 
 solc --overwrite --abi contracts/IdentityFactory.sol -o temp 
+solc --overwrite --abi contracts/Registry.sol -o temp 
 mkdir -p resources abi
 # AdexCore abi
 mv temp/AdExCore.abi abi/AdExCore.json
 mv temp/Identity.abi abi/Identity.json  
 mv temp/IdentityFactory.abi abi/IdentityFactory.json
+mv temp/Registry.abi abi/Registry.json
 # AdexCore bytecode
 bytecode="`cat temp/AdExCore.bin`"; # read file contents into var
 echo "\"$bytecode\"" > "resources/bytecode/AdExCore.json" # write to file
