c6fcac66d4d0c8581e667bc15f2ea86f5a274d72
Ivo Georgiev <ivo@strem.io>
2019-11-15 16:44:53 +0200

    Staking.sol: use BondState struct, saves 5k gas per bond cycle

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -28,6 +28,13 @@ contract Staking {
 	using SafeMath for uint;
 	using BondLibrary for BondLibrary.Bond;
 
+	struct BondState {
+		bool active;
+		uint64 slashedAtStart;
+		uint64 willUnlock;
+	}
+
+	// could be 2**64 too, since we use uint64
 	uint constant MAX_SLASH = 10 ** 18;
 	uint constant TIME_TO_UNBOND = 30 days;
 
@@ -37,9 +44,7 @@ contract Staking {
 	mapping (bytes32 => uint) public totalFunds;
 	mapping (bytes32 => uint) public slashPoints;
 	// Addressed by bondId
-	mapping (bytes32 => bool) public bondIsActive;
-	mapping (bytes32 => uint) public bondSlashedAtOpen;
-	mapping (bytes32 => uint) public bondWillUnlock;
+	mapping (bytes32 => BondState) public bonds;
 
 	constructor(address token, address slasher) public {
    		tokenAddr = token;
@@ -59,39 +64,37 @@ contract Staking {
 
 	function addBond(BondLibrary.Bond memory bond) public {
 		bytes32 id = bond.hash(msg.sender);
-		require(!bondIsActive[id], 'BOND_ALREADY_ACTIVE');
-		bondIsActive[id] = true;
-		bondSlashedAtOpen[id] = slashPoints[bond.poolId];
+		require(!bonds[id].active, 'BOND_ALREADY_ACTIVE');
+		bonds[id] = BondState({
+			active: true,
+			slashedAtStart: uint64(slashPoints[bond.poolId]),
+			willUnlock: 0
+		});
 		totalFunds[bond.poolId] = totalFunds[bond.poolId].add(bond.amount);
 		SafeERC20.transferFrom(tokenAddr, msg.sender, address(this), bond.amount);
 	}
 
 	function requestUnbond(BondLibrary.Bond memory bond) public {
 		bytes32 id = bond.hash(msg.sender);
-		require(bondIsActive[id], 'BOND_NOT_ACTIVE');
-		bondWillUnlock[id] = now + TIME_TO_UNBOND;
+		require(bonds[id].active, 'BOND_NOT_ACTIVE');
+		bonds[id].willUnlock = uint64(now + TIME_TO_UNBOND);
 	}
 
 	function unbond(BondLibrary.Bond memory bond) public {
 		bytes32 id = bond.hash(msg.sender);
 		// redundant
-		// require(bondIsActive[id]);
-		require(bondWillUnlock[id] > 0 && now > bondWillUnlock[id], 'BOND_NOT_UNLOCKED');
+		// require(bonds[id].active);
+		require(bonds[id].willUnlock > 0 && now > bonds[id].willUnlock, 'BOND_NOT_UNLOCKED');
 		uint amount = getWithdrawAmount(bond);
-		bondIsActive[id] = false;
-		bondWillUnlock[id] = 0;
-		if (bondSlashedAtOpen[id] > 0) {
-			bondSlashedAtOpen[id] = 0;
-		}
+		delete bonds[id];
 		totalFunds[bond.poolId] = totalFunds[bond.poolId].sub(amount);
 		SafeERC20.transfer(tokenAddr, msg.sender, amount);
 	}
 
 	function getWithdrawAmount(BondLibrary.Bond memory bond) public view returns (uint) {
-		// TODO fix this .hash() perhaps
-		// return (MAX_SLASH - slashPoints[bond.poolId]) / (MAX_SLASH - bondSlashedAtOpen[bond.hash(msg.sender)]) * bond.amount
+		// @TODO fix this .hash() perhaps
 		return (MAX_SLASH.sub(slashPoints[bond.poolId]))
 			.mul(bond.amount)
-			.div(MAX_SLASH.sub(bondSlashedAtOpen[bond.hash(msg.sender)]));
+			.div(MAX_SLASH.sub(uint(bonds[bond.hash(msg.sender)].slashedAtStart)));
 	}
 }
