71dd58c984028ea128e54cc42d24d0447576ba1e
Ivo Georgiev <ivo@adex.network>
2019-11-17 12:55:50 +0200

    TestStaking: more complex case where we take out one of the bonds

--- test/TestStaking.js
+++ test/TestStaking.js
@@ -90,8 +90,8 @@ contract('Staking', function(accounts) {
 	})
 
 	it('bonds are slashed proportionally based on their bond/unbond time', async function() {
-		const poolId = '0x0202020202020202020202020202020202020202020202020202020202022222'
-		const sum = (a, b) => a+b
+		const poolId = '0x0202020202020202020202020202020202020202020202020202020202222222'
+		const sum = (a, b) => a + b
 
 		// the max slash pts are 10**18
 		// so we will slash it 3 times, all with 5% (10**18*0.05, and then multiplying that by 0.95)
@@ -99,8 +99,8 @@ contract('Staking', function(accounts) {
 		// since those bonds are by the same sender, they need to have different amounts so that they
 		// are understood as different bonds (bondId is derived from the amount/poolId)
 		const bonds = [
-			// this one will suffer 2 slashes cause it will be unbonded after two
-			//[100000000, poolId],
+			// this one will suffer 1 slash cause it will be unbonded after the first one
+			[133000000, poolId],
 			// this one would suffer all 3 slashes
 			[120000000, poolId],
 			// this one will suffer 2 slashes
@@ -108,26 +108,61 @@ contract('Staking', function(accounts) {
 			// this one will suffer 1 slash
 			[130000000, poolId],
 			// this one will suffer 0 slashes
-			[399000000, poolId],
+			[399000000, poolId]
 		]
-		const bondExpected = [
-			bonds[0][0] * 0.95 * 0.95 * 0.95,
-			bonds[1][0] * 0.95 * 0.95,
-			bonds[2][0] * 0.95,
-			bonds[3][0],
+		const bondsExpected = [
+			bonds[0][0] * 0.95,
+			bonds[1][0] * 0.95 * 0.95 * 0.95,
+			bonds[2][0] * 0.95 * 0.95,
+			bonds[3][0] * 0.95,
+			bonds[4][0]
 		]
-		// @TODO unbond one of them
-		await token.setBalanceTo(userAddr, bonds.map(bond => bond[0]).reduce(sum, 0))
-		await (await staking.addBond(bonds[0], { gasLimit })).wait()
-		await (await stakingWithSlasher.slash(poolId, slashes[0], { gasLimit })).wait()
-		await (await staking.addBond(bonds[1], { gasLimit })).wait()
-		await (await stakingWithSlasher.slash(poolId, slashes[1], { gasLimit })).wait()
-		await (await staking.addBond(bonds[2], { gasLimit })).wait()
-		await (await stakingWithSlasher.slash(poolId, slashes[2], { gasLimit })).wait()
-		await (await staking.addBond(bonds[3], { gasLimit })).wait()
 
-		const amounts = await Promise.all(bonds.map(bond => staking.getWithdrawAmount(bond)))
-		assert.deepEqual(amounts.map(x => x.toNumber()), bondExpected, 'amounts are as expected')
-		assert.equal(await staking.totalFunds(poolId), bondExpected.reduce(sum, 0), 'totalFunds is as expected')
+		// prepare the token amount
+		const totalAmount = bonds.map(bond => bond[0]).reduce(sum, 0)
+		await token.setBalanceTo(userAddr, totalAmount)
+
+		// the first bond will be unbonded immediately, and withdrawn after the second slash
+		await (await staking.addBond(bonds[0], { gasLimit })).wait()
+		await (await staking.requestUnbond(bonds[0], { gasLimit })).wait()
+
+		await (await staking.addBond(bonds[1], { gasLimit })).wait()
+		await (await stakingWithSlasher.slash(poolId, slashes[0], { gasLimit })).wait()
+
+		// now we will take out bonds[0]
+		await moveTime(web3, DAY_SECONDS * 31)
+		assert.equal(
+			await staking.getWithdrawAmount(bonds[0]),
+			bondsExpected[0],
+			'getWithdrawAmount is correct'
+		)
+		const unbondReceipt = await (await staking.unbond(bonds[0], { gasLimit })).wait()
+		assert.equal(
+			parseInt(unbondReceipt.events[0].data, 16),
+			bondsExpected[0],
+			'the amount withdrawn is correct'
+		)
+		// assert.equal(staking.getWithdrawAmount(bonds[0]), 0, 'no more to withdraw')
+		const remainingBonds = bonds.slice(1)
+		const remainingBondsExpected = bondsExpected.slice(1)
+
+		// continue as planned
+		await (await staking.addBond(bonds[2], { gasLimit })).wait()
+		await (await stakingWithSlasher.slash(poolId, slashes[1], { gasLimit })).wait()
+		await (await staking.addBond(bonds[3], { gasLimit })).wait()
+		await (await stakingWithSlasher.slash(poolId, slashes[2], { gasLimit })).wait()
+		await (await staking.addBond(bonds[4], { gasLimit })).wait()
+
+		const amounts = await Promise.all(remainingBonds.map(bond => staking.getWithdrawAmount(bond)))
+		assert.deepEqual(
+			amounts.map(x => x.toNumber()),
+			remainingBondsExpected,
+			'amounts are as expected'
+		)
+		assert.equal(
+			await staking.totalFunds(poolId),
+			remainingBondsExpected.reduce(sum, 0),
+			'totalFunds is as expected'
+		)
 	})
 })
