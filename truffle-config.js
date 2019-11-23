faeb9c2092b17797195000f7c1aee9456939969a
Ivo Georgiev <ivo@adex.network>
2019-11-23 22:18:43 +0000

    make tests pass with the new truffle

--- truffle-config.js
+++ truffle-config.js
@@ -46,13 +46,13 @@ module.exports = {
 		// tab if you use this network and you must also set the `host`, `port` and `network_id`
 		// options below to some value.
 		//
-		// development: {
-		//  host: 'localhost',
-		//  port: 8545,
-		//  network_id: '*', // Match any network id
-		//  gas: 4500000,
-		//  gasPrice: 10000000000,
-		// },
+		development: {
+		  host: 'localhost',
+		  port: 8545,
+		  network_id: '*', // Match any network id
+		  gas: 4500000,
+		  gasPrice: 10000000000,
+		},
 		// development: {
 		//  host: "127.0.0.1",     // Localhost (default: none)
 		//  port: 8545,            // Standard Ethereum port (default: none)
