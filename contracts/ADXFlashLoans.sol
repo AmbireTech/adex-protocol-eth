068a222c799c50901f52a1e51d897452cdffec01
Ivo Georgiev <ivo@adex.network>
2020-07-31 19:52:50 +0300

    ADXFlashLoans reworked and tests added

--- contracts/ADXFlashLoans.sol
+++ contracts/ADXFlashLoans.sol
@@ -1,23 +1,15 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "./libs/SafeERC20.sol";
+import "./Identity.sol";
 
 contract ADXFlashLoans {
-	// Very important that this contract does not have any storage
-	function flash(GeneralERC20 token, uint amount, address to, bytes memory data) public {
+	function flash(GeneralERC20 token, uint amount, Identity receiver, Identity.Transaction[] memory txns, bytes32[3][] memory signatures) public {
 		uint bal = token.balanceOf(address(this));
-		token.transfer(to, amount);
-		assembly {
-			let result := delegatecall(gas(), to, add(data, 0x20), mload(data), 0, 0)
-			switch result case 0 {
-				let size := returndatasize()
-				let ptr := mload(0x40)
-				returndatacopy(ptr, 0, size)
-				revert(ptr, size)
-			}
-			default {}
-		}
+		token.transfer(address(receiver), amount);
+		receiver.execute(txns, signatures);
 		require(token.balanceOf(address(this)) == bal, 'FLASHLOAN_NOT_RETURNED');
 	}
 }
