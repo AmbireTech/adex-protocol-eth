0dba1eccd6c83d0475d37a2d3475c3909b545d78
Ivo Georgiev <ivo@adex.network>
2020-07-31 11:50:53 +0300

    ADXToken: split out ADXFlashLoans

--- contracts/ADXToken.sol
+++ contracts/ADXToken.sol
@@ -4,25 +4,6 @@ pragma solidity ^0.6.12;
 import "./libs/SafeMath.sol";
 import "./libs/SafeERC20.sol";
 
-contract ADXFlashLoans {
-	// Very important that this contract does not have any storage
-	function flash(GeneralERC20 token, uint amount, address to, bytes memory data) public {
-		uint bal = token.balanceOf(address(this));
-		token.transfer(to, amount);
-		assembly {
-			let result := delegatecall(gas(), to, add(data, 0x20), mload(data), 0, 0)
-			switch result case 0 {
-				let size := returndatasize()
-				let ptr := mload(0x40)
-				returndatacopy(ptr, 0, size)
-				revert(ptr, size)
-			}
-			default {}
-		}
-		require(token.balanceOf(address(this)) == bal, 'FLASHLOAN_NOT_RETURNED');
-	}
-}
-
 contract ADXSupplyController {
 	enum GovernanceLevel { None, Mint, All }
 	mapping (address => uint8) governance;
