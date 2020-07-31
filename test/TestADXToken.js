6757a1516164c294408987175b770268c223f8c6
Ivo Georgiev <ivo@strem.io>
2020-07-31 17:54:13 +0300

    TestADXToken: add transfer test

--- test/TestADXToken.js
+++ test/TestADXToken.js
@@ -12,6 +12,7 @@ contract('ADXToken', function(accounts) {
 	const userAddr = accounts[1]
 	// @TODO contract
 	const supplyCtrlAddr = accounts[2]
+	const anotherUser = accounts[3]
 
 	let prevToken
 	let adxToken
@@ -28,9 +29,10 @@ contract('ADXToken', function(accounts) {
 	it('swap previous tokens', async function() {
 		await prevToken.setBalanceTo(userAddr, 10000)
 		const receipt = await (await adxToken.swap(10000)).wait()
+		const expectedAmnt = bigNumberify('1000000000000000000')
 		assert.deepEqual(
 			await adxToken.balanceOf(userAddr),
-			bigNumberify('1000000000000000000'),
+			expectedAmnt,
 			'migrated amount is correct'
 		)
 		assert.equal(
@@ -40,5 +42,24 @@ contract('ADXToken', function(accounts) {
 		)
 		assert.equal(receipt.events.length, 2, '2 Transfer events')
 		assert.ok(receipt.gasUsed.toNumber() < 100000, 'gas usage is OK')
+
+		assert.deepEqual(await adxToken.totalSupply(), expectedAmnt, 'total supply is reflected')
 	})
+	
+	it('transfer some tokens', async function() {
+		const transferredAmount = bigNumberify('550000000000000000')
+		const receipt = await (await adxToken.transfer(anotherUser, transferredAmount)).wait()
+		assert.deepEqual(
+			await adxToken.balanceOf(anotherUser),
+			transferredAmount,
+			'transferredAmount amount is correct'
+		)
+		assert.equal(receipt.events.length, 1, '1 event')
+		assert.equal(receipt.events[0].event, 'Transfer', 'event is a transfer')
+		assert.deepEqual(receipt.events[0].args.amount, transferredAmount, 'transfer amount is OK')
+		assert.ok(receipt.gasUsed.toNumber() < 56000, 'gas usage is OK')
+	})
+
+	// @TODO supply controller minting
+	// @TODO flash loans
 })
