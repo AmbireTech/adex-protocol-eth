d8442f68b9827d6d32eb41444ed1c23d7a0c563c
Ivo Georgiev <ivo@strem.io>
2020-08-02 10:23:30 +0300

    truffle-config: increase gas for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -50,7 +50,7 @@ module.exports = {
 			host: 'localhost',
 			port: 8545,
 			network_id: '*', // Match any network id
-			gas: 4500000,
+			gas: 6000000,
 			gasPrice: 10000000000
 		},
 		// development: {
