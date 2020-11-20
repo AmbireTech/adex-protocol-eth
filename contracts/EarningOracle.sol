1b0a9ef8f782d1d69b1704c8d971565cd5e326a7
samparsky <8148384+samparsky@users.noreply.github.com>
2020-11-20 07:21:18 +0300

    add: earning oracle impl & test

---
+++ contracts/EarningOracle.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "./interfaces/IAdExCore.sol";
+import "./interfaces/IEarningOracle.sol";
+import "./libs/ChannelLibrary.sol";
+
+
+contract EarningOracle is IEarningOracle {
+    IAdExCore public core;
+
+    mapping (address => uint) internal totalEarnings;
+    mapping (bytes32 => bool) public tallied;
+
+    constructor (IAdExCore _core) public {
+        core = _core;
+    }
+
+    function bulkUpdate(bytes32[] calldata channelIds, address[] calldata earners) external {
+        for(uint i = 0; i < channelIds.length; i++) {
+            bytes32 channelId = channelIds[i];
+            address earner = earners[i];
+            bytes32 tallyId = keccak256(
+                abi.encode(
+			        address(this), 
+                    channelId, 
+                    earner
+                )
+            );
+            
+            require(tallied[tallyId] == false, 'ALREADY_TALLIED');
+            require(core.states(channelId) == ChannelLibrary.State.Expired, 'CHANNEL_NOT_EXPIRED');
+
+            tallied[tallyId] = true;
+            totalEarnings[earner] += core.withdrawnPerUser(channelId, earner);
+        }
+    }
+
+   function getTotalEarning(address earner) external view override returns (uint) {
+       return totalEarnings[earner];
+   }
+}
