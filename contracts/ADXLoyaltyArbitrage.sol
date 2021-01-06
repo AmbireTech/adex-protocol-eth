208a4f7367584b8f7cf4a2f44552dadaae85e6a3
Ivo Georgiev <ivo@adex.network>
2021-01-05 19:26:07 -0500

    ADXLoyaltyArbitrage compiles

--- contracts/ADXLoyaltyArbitrage.sol
+++ contracts/ADXLoyaltyArbitrage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
@@ -98,10 +98,10 @@ contract ADXLoyaltyArb is Ownable {
 	IERC20 public constant ADXL = IERC20(0xd9A4cB9dc9296e111c66dFACAb8Be034EE2E1c2C);
 
 	constructor() public {
-		ADX.approve(address(uniswap), uint(-1));
-		ADX.approve(address(ADXL), uint(-1));
-		ADXL.approve(address(uniswap), uint(-1));
-		ADXL.approve(address(ADXL), uint(-1));
+		ADX.approve(address(uniswap), ~uint256(0));
+		ADX.approve(address(ADXL), uint256(0));
+		ADXL.approve(address(uniswap), uint256(0));
+		ADXL.approve(address(ADXL), uint256(0));
 	}
 
 	// No need to check success here, no safeerc20
