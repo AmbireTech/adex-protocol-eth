78520ff19497189da37ade2acc14bc4a4253b94e
Ivo Georgiev <ivo@adex.network>
2021-01-22 21:54:08 +0100

    Interfaces: add IADXToken

--- contracts/ADXLoyaltyPool.sol
+++ contracts/ADXLoyaltyPool.sol
@@ -1,18 +1,7 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
-interface ISupplyController {
-	function mint(address token, address owner, uint amount) external;
-}
-
-interface IADXToken {
-	function transfer(address to, uint256 amount) external returns (bool);
-	function transferFrom(address from, address to, uint256 amount) external returns (bool);
-	function approve(address spender, uint256 amount) external returns (bool);
-	function balanceOf(address spender) external view returns (uint);
-	function allowance(address owner, address spender) external view returns (uint);
-	function supplyController() external view returns (ISupplyController);
-}
+import "./interfaces/IADXToken.sol";
 
 contract ADXLoyaltyPoolToken {
 	// ERC20 stuff
---
+++ contracts/interfaces/IADXToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+interface ISupplyController {
+	function mintIncentive(address addr) external;
+	function mintableIncentive(address addr) external view returns (uint);
+	function mint(address token, address owner, uint amount) external;
+}
+
+interface IADXToken {
+	function transfer(address to, uint256 amount) external returns (bool);
+	function transferFrom(address from, address to, uint256 amount) external returns (bool);
+	function approve(address spender, uint256 amount) external returns (bool);
+	function balanceOf(address spender) external view returns (uint);
+	function allowance(address owner, address spender) external view returns (uint);
+	function totalSupply() external returns (uint);
+	function supplyController() external view returns (ISupplyController);
+	function changeSupplyController(address newSupplyController) external;
+	function mint(address owner, uint amount) external;
+}
