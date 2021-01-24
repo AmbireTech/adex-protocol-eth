f934f54a94e19c9beb9e31bdb8acdb2651adc908
Ivo Georgiev <ivo@strem.io>
2021-01-24 23:25:50 +0100

    v5: cleanup

--- contracts/Guardian.sol
+++ contracts/Guardian.sol
@@ -50,10 +50,8 @@ contract Guardian {
 		// @TODO court
 		return channel.leader;
 	}
-	// @TODO pass in channel
-	// @TODO staking pool claim() interface
 	
-	// @TODO; should we cache blame
+	// @TODO; should we cache blame?
 	function getRefund(IOUTPACE outpace, IOUTPACE.Channel calldata channel, Campaign calldata campaign) external {
 		bytes32 channelId = keccak256(abi.encode(channel));
 		require(channelId == campaign.channelId, 'channelId matches');
@@ -64,7 +62,6 @@ contract Guardian {
 
 		// code-wise, perhaps make a separate method that gets executed only once that updates all the refund state (for a channel) initially to calculate who's to blame and save what's taken already; perhaps it would be best to liquidate the adx at once now
 
-		// @TODO encode vs encodepacked
 		bytes32 depositId = keccak256(abi.encode(campaign));
 		require(!refunds[channelId][depositId], 'refund already received');
 		refunds[channelId][depositId] = true;
@@ -81,18 +78,17 @@ contract Guardian {
 			outpace.close(channel);
 			// assign blame now
 			//blame[channel] = validator // call court if available
-			// @TODO set liquidation allowance
 		}
 
 		if (remainingFunds == 0) {
-			// @TODO optimizing the case in which remaining has ran out - then we just claim directly to the recipient
+			// Optimizing the case in which remaining has ran out - then we just claim directly to the recipient (campaign.creator)
 			address poolAddr = poolForValidator[getBlame(channel)];
 			require(poolAddr != address(0), 'no pool');
 			IStakingPool(poolAddr).claim(channel.tokenAddr, campaign.creator, deposit);
 		} else {
 			if (remainingFunds < deposit) {
-				// Note the liquidation itself is a resposibility of the staking contract - because some staking pools might hold LP tokens
-				// @TODO get the staking pool - use an internal registry?
+				// Note the liquidation itself is a resposibility of the staking contract
+				// the rationale is that some staking pools might hold LP tokens, so the liquidation logic should be in the pool
 				address poolAddr = poolForValidator[getBlame(channel)];
 				require(poolAddr != address(0), 'no pool');
 				IStakingPool(poolAddr).claim(channel.tokenAddr, address(this), deposit-remainingFunds);
