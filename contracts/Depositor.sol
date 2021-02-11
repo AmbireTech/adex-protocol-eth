b3a0ada68f76593167f74012414875c1990584d7
Ivo Georgiev <ivo@strem.io>
2021-02-11 19:15:27 +0200

    v5.1 CREATE2 depositor added

---
+++ contracts/Depositor.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+import "./OUTPACE.sol";
+import "./libs/SafeERC20.sol";
+
+contract ADXDistribution {
+	constructor(OUTPACE outpace, OUTPACE.Channel memory channel, address depositor) {
+		uint amount = GeneralERC20(channel.tokenAddr).balanceOf(address(this));
+		SafeERC20.approve(channel.tokenAddr, address(outpace), amount);
+		outpace.deposit(channel, depositor, amount);
+		assembly {
+			selfdestruct(0)
+		}
+	}
+}
+
