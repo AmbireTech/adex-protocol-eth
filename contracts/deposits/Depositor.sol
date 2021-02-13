b77afeafd228fc921cc6ddc0646e7d788e7dc181
Ivo Georgiev <ivo@strem.io>
2021-02-13 15:42:46 +0200

    v5.1 sweeper done

--- contracts/deposits/Depositor.sol
+++ contracts/deposits/Depositor.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
-import "./OUTPACE.sol";
-import "./libs/SafeERC20.sol";
+import "../OUTPACE.sol";
+import "../libs/SafeERC20.sol";
 
-contract ADXDistribution {
+contract Depositor {
 	constructor(OUTPACE outpace, OUTPACE.Channel memory channel, address depositor) {
 		uint amount = GeneralERC20(channel.tokenAddr).balanceOf(address(this));
 		SafeERC20.approve(channel.tokenAddr, address(outpace), amount);
