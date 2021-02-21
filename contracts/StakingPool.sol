4e64bec07fb6ca6260141fba1980177353436d49
Ivo Georgiev <ivo@strem.io>
2021-02-21 18:06:18 +0200

    StakingPool: no need for governance

--- contracts/StakingPool.sol
+++ contracts/StakingPool.sol
@@ -108,7 +108,6 @@ contract StakingPool {
 	IChainlink public constant ADXUSDOracle = IChainlink(0x231e764B44b2C1b7Ca171fa8021A24ed520Cde10);
 
 	IADXToken public ADXToken;
-	mapping (address => bool) public governance;
 	address public guardian;
 	address public validator;
 
@@ -124,12 +123,11 @@ contract StakingPool {
 	}
 
 	// claims/penalizations limits
-	uint public MAX_DAILY_PENALTIES_PROMILLES;
+	uint public immutable MAX_DAILY_PENALTIES_PROMILLES;
 	uint public limitLastReset;
 	uint public limitRemaining;
 
 	// Staking pool events
-	event LogSetGovernance(address indexed addr, bool hasGovt, uint time);
 	// LogLeave/LogWithdraw must begin with the UnbondCommitment struct
 	// @TODO can we embed the struct itself?
 	event LogLeave(address indexed owner, uint shares, uint unlockAt, uint maxTokens);
@@ -143,12 +141,11 @@ contract StakingPool {
 		ADXToken = token;
 		guardian = _guardian;
 		validator = _validator;
-		governance[msg.sender] = true;
-		emit LogSetGovernance(msg.sender, true, block.timestamp);
 
 		// max daily penalties
 		require(dailyPenalties <= 500, 'DAILY_PENALTY_TOO_LARGE');
 		MAX_DAILY_PENALTIES_PROMILLES = dailyPenalties;
+
 		// EIP 2612
 		uint chainId;
 		assembly {
@@ -163,14 +160,6 @@ contract StakingPool {
 				address(this)
 			)
 		);
-	}
-
-	// Governance functions
-	// @TODO: consider a single owner rather than multiple govt?
-	function setGovernance(address addr, bool hasGovt) external {
-		require(governance[msg.sender], 'NOT_GOVERNANCE');
-		governance[addr] = hasGovt;
-		emit LogSetGovernance(addr, hasGovt, block.timestamp);
 	}
 
 	// Pool stuff
