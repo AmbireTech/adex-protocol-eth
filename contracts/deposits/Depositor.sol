f1a8c633e822eb0e31e5bd14f3792fc00917235f
Ivo Georgiev <ivo@strem.io>
2021-02-22 22:30:16 +0200

    use IERC20 rather than GeneralERC20

--- contracts/deposits/Depositor.sol
+++ contracts/deposits/Depositor.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "../OUTPACE.sol";
-import "../libs/SafeERC20.sol";
+import "../interfaces/IERC20.sol";
 
 contract Depositor {
 	constructor(OUTPACE outpace, OUTPACE.Channel memory channel, address depositor) {
-		uint amount = GeneralERC20(channel.tokenAddr).balanceOf(address(this));
+		uint amount = IERC20(channel.tokenAddr).balanceOf(address(this));
 		SafeERC20.approve(channel.tokenAddr, address(outpace), amount);
 		outpace.deposit(channel, depositor, amount);
 		assembly {
