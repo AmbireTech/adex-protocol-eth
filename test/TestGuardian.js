0392ffdb9d7fc7b9c4aed086708374acd25eba77
samparsky <8148384+samparsky@users.noreply.github.com>
2021-03-11 08:50:49 +0200

    add: TestGuardian & TestStakingPool

---
+++ test/TestGuardian.js
@@ -0,0 +1,10 @@
+const { providers, Contract } = require('ethers')
+const { bigNumberify } = require('ethers').utils
+
+const { expectEVMError } = require('./')
+
+const Guardian = artifacts.require('Guardian')
+
+contract('Guardian', function(accounts) {
+
+})
