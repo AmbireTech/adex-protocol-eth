a3663a15b7cb51a7d3e08810477be0c6bef7738c
samparsky <8148384+samparsky@users.noreply.github.com>
2021-03-22 10:11:43 +0200

    feat: add penalize test case TestStakingPool

--- contracts/mocks/MockChainlink.sol
+++ contracts/mocks/MockChainlink.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract MockChainlink {
-    function lastestAnswer() external pure returns (uint256) {
+    function latestAnswer() external pure returns (uint256) {
         return 1e8;
     }
 }
