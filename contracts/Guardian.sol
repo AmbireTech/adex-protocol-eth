9eb5f0b83632a5fedc646c7d6ce20251a4a8b168
Ivo Georgiev <ivo@adex.network>
2021-03-29 17:02:49 +0300

    Guardian: do not call claim() if there is no interest

--- contracts/Guardian.sol
+++ contracts/Guardian.sol
@@ -70,8 +70,9 @@ contract Guardian {
 		// Do not send interest if there is no lastStateRoot (channel has not been used)
 		// cause without it, it's possible to open non-legit channels with real validators, let them expire and try to claim the interest
 		// Only apply the interest if the channel has been used and there's a pool from which to get it, and there's no opt out
-		if (lastStateRoot != bytes32(0) && poolAddr != address(0) && !skipInterest) {
-			IStakingPool(poolAddr).claim(channel.tokenAddr, spender, (refundablePrincipal * refundInterestPromilles[blamed]) / 1000);
+		uint interest = refundInterestPromilles[blamed];
+		if (!skipInterest && poolAddr != address(0) && interest != 0 && lastStateRoot != bytes32(0)) {
+			IStakingPool(poolAddr).claim(channel.tokenAddr, spender, (refundablePrincipal * interest) / 1000);
 		}
 	}
 }
