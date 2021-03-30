b6e163955199a1022ff5af0a733ba3c099b3321a
Ivo Georgiev <ivo@strem.io>
2021-03-30 15:49:58 +0300

    Revert "Revert to a hardcoded token on the SupplyController"
    This reverts commit 57999972d4f738bcc18cdfe2264305b265b85b68.

--- contracts/SupplyController.sol
+++ contracts/SupplyController.sol
@@ -9,8 +9,7 @@ contract ADXSupplyController {
 	uint public constant CAP = 150000000 * 1e18;
 	// This amount was burned on purpose when migrating from Tom pool 2 (Staking with token 0xade) to Tom pool 3 (StakingPool with token 0xade)
 	uint public immutable BURNED_MIN = 35000000 * 1e18;
-	// hardcoded cause constructor arguments + deploying via factory does not play well with etherscan
-	IADXToken public constant ADX = IADXToken(0xADE00C28244d5CE17D72E40330B1c318cD12B7c3);
+	IADXToken public immutable ADX;
 
 	mapping (address => uint8) public governance;
 	// Some addresses (eg StakingPools) are incentivized with a certain allowance of ADX per year
@@ -18,8 +17,9 @@ contract ADXSupplyController {
 	// Keep track of when incentive tokens were last minted for a given addr
 	mapping (address => uint) public incentiveLastMint;
 
-	constructor() {
+	constructor(IADXToken token) {
 		governance[msg.sender] = uint8(GovernanceLevel.All);
+		ADX = token;
 	}
 
 	function changeSupplyController(address newSupplyController) external {
