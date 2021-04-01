3c19fea4c98fe65a7bd799e35586fbc41883453a
Ivo Georgiev <ivo@adex.network>
2021-04-01 21:17:41 +0300

    Gasless: flip token and pool for better gas usage

--- contracts/extra/Gasless.sol
+++ contracts/extra/Gasless.sol
@@ -13,13 +13,13 @@ contract GaslessSweeper {
 	function sweep(IStakingPool pool, address[] memory depositors) external {
 		address token = pool.ADXToken();
 		for (uint i = 0; i < depositors.length; i++) {
-			new GaslessDepositor{ salt: bytes32(0) }(pool, token, depositors[i]);
+			new GaslessDepositor{ salt: bytes32(0) }(token, pool, depositors[i]);
 		}
 	}
 }
 
 contract GaslessDepositor {
-	constructor(IStakingPool pool, address token, address depositor) {
+	constructor(address token,IStakingPool pool, address depositor) {
 		uint amount = IERC20(token).balanceOf(address(this));
 		SafeERC20.approve(token, address(pool), amount);
 		pool.enterTo(depositor, amount);
