ef04493e63123b005a3ff182d989323f0ab5839b
Ivo Georgiev <ivo@strem.io>
2021-04-05 16:52:24 +0300

    build script updated

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 set -e
 
 mkdir -p temp 
-solc --abi --bin contracts/AdExCore.sol -o temp 
+solc --abi --bin contracts/OUTPACE.sol -o temp 
 solc --overwrite --abi contracts/Identity.sol -o temp 
 solc --overwrite --abi contracts/IdentityFactory.sol -o temp 
-solc --overwrite --abi contracts/Staking.sol -o temp 
+solc --overwrite --abi contracts/StakignPool.sol -o temp 
 mkdir -p resources abi
 # AdexCore abi
-mv temp/AdExCore.abi abi/AdExCore.json
+mv temp/OUTPACE.abi abi/OUTPACE.json
 mv temp/Identity.abi abi/Identity.json  
 mv temp/IdentityFactory.abi abi/IdentityFactory.json
-mv temp/Staking.abi abi/Staking.json
+mv temp/StakignPool.abi abi/StakignPool.json
 # AdexCore bytecode
-bytecode="`cat temp/AdExCore.bin`"; # read file contents into var
-echo "\"$bytecode\"" > "resources/bytecode/AdExCore.json" # write to file
+bytecode="`cat temp/OUTPACE.bin`"; # read file contents into var
+echo "\"$bytecode\"" > "resources/bytecode/OUTPACE.json" # write to file
 rm -r temp/
