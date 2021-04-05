c0cdf4038f6f013ffcae234b69809fa3d4eefc8e
Ivo Georgiev <ivo@strem.io>
2021-04-05 16:56:21 +0300

    build.sh: fix typo

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ mkdir -p temp
 solc --abi --bin contracts/OUTPACE.sol -o temp 
 solc --overwrite --abi contracts/Identity.sol -o temp 
 solc --overwrite --abi contracts/IdentityFactory.sol -o temp 
-solc --overwrite --abi contracts/StakignPool.sol -o temp 
+solc --overwrite --abi contracts/StakingPool.sol -o temp 
 mkdir -p resources abi
 # AdexCore abi
 mv temp/OUTPACE.abi abi/OUTPACE.json
 mv temp/Identity.abi abi/Identity.json  
 mv temp/IdentityFactory.abi abi/IdentityFactory.json
-mv temp/StakignPool.abi abi/StakignPool.json
+mv temp/StakingPool.abi abi/StakingPool.json
 # AdexCore bytecode
 bytecode="`cat temp/OUTPACE.bin`"; # read file contents into var
 echo "\"$bytecode\"" > "resources/bytecode/OUTPACE.json" # write to file
