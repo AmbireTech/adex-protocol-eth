b497b5df9047d32f2974f5a07f488be3e13a7a70
Ivo Georgiev <ivo@strem.io>
2021-05-07 21:45:23 +0300

    Zapper mostly done, only aave and approvals remaining

--- contracts/wallet/Zapper.sol
+++ contracts/wallet/Zapper.sol
@@ -10,8 +10,56 @@ interface IAaveLendingPool {
   ) external;
 }
 
+// Full interface here: https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/IUniswapV2Router01.sol
+interface IUniswapSimple {
+        function WETH() external pure returns (address);
+        function swapTokensForExactTokens(
+                uint amountOut,
+                uint amountInMax,
+                address[] calldata path,
+                address to,
+                uint deadline
+        ) external returns (uint[] memory amounts);
+	function swapExactTokensForTokens(
+		uint amountIn,
+		uint amountOutMin,
+		address[] calldata path,
+		address to,
+		uint deadline
+	) external returns (uint[] memory amounts);
+}
+
+
 contract WalletZapper {
-	function exchange() external {
+	// @TODO: constructor
+
+
+	struct Trade {
+		IUniswapSimple router;
+		// @TODO should there be a trade type
+		uint amountIn;
+		uint amountOutMin;
+		address[] path;
+		bool wrap;
+	}
+
+	function exchange(address[] calldata assetsToUnwrap, Trade[] memory trades) external {
+		// @TODO: unwrap
+		// @TODO: should those be vars
+		address to = msg.sender;
+		uint deadline = block.timestamp;
+		// @TODO: should trades.length be assigned to a local var? if so, should this be applied to other places in v5 as well?
+		for (uint i=0; i!=trades.length; i++) {
+			Trade memory trade = trades[i];
+			if (!trade.wrap) {
+				trade.router.swapExactTokensForTokens(trade.amountIn, trade.amountOutMin, trade.path, to, deadline);
+			} else {
+				trade.router.swapExactTokensForTokens(trade.amountIn, trade.amountOutMin, trade.path, address(this), deadline);
+				// @TODO aaveLendingPool.deposit(trade.path[last], outAmount, to, refCode);
+			}
+		}
+		// @TODO: wrapping
+
 	}
 
 	function wrapLending() external {
