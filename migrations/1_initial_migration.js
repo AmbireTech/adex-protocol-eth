3de44097197074b494d5f462e79dd10bf4563a9f
Ivo Georgiev <ivo@adex.network>
2021-08-02 14:28:00 +0200

    Deploy wallet contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,19 @@
-const Migrations = artifacts.require('Migrations')
-const OUTPACE = artifacts.require('OUTPACE')
+// const OUTPACE = artifacts.require('OUTPACE')
+const IdentityFactory = artifacts.require('IdentityFactory')
+const Identity = artifacts.require('Identity')
+const Zapper = artifacts.require('WalletZapper')
 
-module.exports = function(deployer) {
-	deployer.deploy(OUTPACE)
-	deployer.deploy(Migrations)
+module.exports = async function(deployer) {
+	// deployer.deploy(OUTPACE)
+	await deployer.deploy(IdentityFactory)
+	await deployer.deploy(Identity, [])
+	// ethereum mainnet: https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts
+	await deployer.deploy(Zapper, '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9', 0, [
+		// uni v2
+		'0x7a250d5630b4cf539739df2c5dacb4c659f2488d',
+		// uni v3
+		'0xE592427A0AEce92De3Edee1F18E0157C05861564',
+		// sushi
+		'0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f'
+	])
 }
