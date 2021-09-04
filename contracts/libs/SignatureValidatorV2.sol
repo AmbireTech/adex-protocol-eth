470f85ff38c92c59bc2c37be1dbbef159f4251a8
Ivo Georgiev <ivo@strem.io>
2021-09-04 07:17:43 +0200

    WIP: new SignatureValidator for v5.2

---
+++ contracts/libs/SignatureValidatorV2.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.8.1;
+
+interface IERC1271Wallet {
+	function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue);
+}
+
+
+
+library LibBytes {
+  using LibBytes for bytes;
+
+  // @TODO: see if we can just set .length = 
+  function trimToSize(bytes memory b, uint newLen)
+    internal
+    pure
+  {
+    require(b.length > newLen, "only shrinking");
+    assembly {
+      mstore(b, newLen)
+    }
+  }
+
+
+  /***********************************|
+  |        Read Bytes Functions       |
+  |__________________________________*/
+
+  /**
+   * @dev Reads a bytes32 value from a position in a byte array.
+   * @param b Byte array containing a bytes32 value.
+   * @param index Index in byte array of bytes32 value.
+   * @return result bytes32 value from byte array.
+   */
+  function readBytes32(
+    bytes memory b,
+    uint256 index
+  )
+    internal
+    pure
+    returns (bytes32 result)
+  {
+    require(
+      b.length >= index + 32,
+      "LibBytes#readBytes32: GREATER_OR_EQUAL_TO_32_LENGTH_REQUIRED"
+    );
+
+    // Arrays are prefixed by a 256 bit length parameter
+    index += 32;
+
+    // Read the bytes32 from array memory
+    assembly {
+      result := mload(add(b, index))
+    }
+    return result;
+  }
+}
+
+
+library SignatureValidator {
+	using LibBytes for bytes;
+
+	enum SignatureMode {
+		NO_SIG,
+		CALLER,
+		EIP712,
+		ETHSIGN,
+		WALLET,
+		// must be at the end
+		UNSUPPORTED
+	}
+
+	// bytes4(keccak256("isValidSignature(bytes32,bytes)"))
+	bytes4 constant internal ERC1271_MAGICVALUE_BYTES32 = 0x1626ba7e;
+
+	function recoverAddr(bytes32 hash, bytes memory sig) internal view returns (address) {
+		// @TODO sig len check
+		uint8 modeRaw = uint8(sig[sig.length - 1]);
+		require(modeRaw < uint8(SignatureMode.UNSUPPORTED), "unsupported sig mode");
+		SignatureMode mode = SignatureMode(modeRaw);
+
+		if (mode == SignatureMode.NO_SIG) {
+			return address(0x0);
+		}
+
+		if (mode == SignatureMode.CALLER) return msg.sender;
+		if (mode == SignatureMode.EIP712 || mode == SignatureMode.ETHSIGN) {
+			// @TODO sig len check
+			bytes32 r = sig.readBytes32(0);
+			bytes32 s = sig.readBytes32(32);
+			uint8 v = uint8(sig[64]);
+			if (mode == SignatureMode.EIP712) return ecrecover(hash, v, r, s);
+			else return ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)), v, r, s);
+		}
+		if (mode == SignatureMode.WALLET) {
+			// @TODO: sig len check
+			IERC1271Wallet wallet = IERC1271Wallet(address(uint160(uint256(sig.readBytes32(sig.length - 32)))));
+			sig.trimToSize(sig.length - 33); // 32 bytes for the addr, 1 byte for the type
+			require(ERC1271_MAGICVALUE_BYTES32 == wallet.isValidSignature(hash, sig), "invalid wallet sig");
+			return address(wallet);
+		}
+		// @TODO: return 0?
+	}
+}
