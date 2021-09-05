7b2e8520e5e442a6ddf9f73d03d598bc05ebc8cb
Ivo Georgiev <ivo@adex.network>
2021-09-05 23:11:24 +0300

    WIP: Identity v5.2: use pure for supportsInterface

--- contracts/Identityv5.2.sol
+++ contracts/Identityv5.2.sol
@@ -145,7 +145,7 @@ contract Identity {
 
 	// EIP 1155 implementation
 	// we pretty much only need to signal that we support the interface and i
-	function supportsInterface(bytes4 interfaceID) external view returns (bool) {
+	function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
 		return
 			interfaceID == 0x01ffc9a7 ||    // ERC-165 support (i.e. `bytes4(keccak256('supportsInterface(bytes4)'))`).
 			interfaceID == 0x4e2312e0;      // ERC-1155 `ERC1155TokenReceiver` support (i.e. `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")) ^ bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`).
