32c8d8a0c2f9b14d68af6b305f0fb22dec1bd425
Ivo Georgiev <ivo@strem.io>
2021-09-05 09:02:39 +0300

    BytesLib separated

--- contracts/libs/SignatureValidatorV2.sol
+++ contracts/libs/SignatureValidatorV2.sol
@@ -1,60 +1,10 @@
 pragma solidity ^0.8.1;
 
+import "./BytesLib.sol";
+
 interface IERC1271Wallet {
 	function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue);
 }
-
-
-
-library LibBytes {
-  using LibBytes for bytes;
-
-  // @TODO: see if we can just set .length = 
-  function trimToSize(bytes memory b, uint newLen)
-    internal
-    pure
-  {
-    require(b.length > newLen, "only shrinking");
-    assembly {
-      mstore(b, newLen)
-    }
-  }
-
-
-  /***********************************|
-  |        Read Bytes Functions       |
-  |__________________________________*/
-
-  /**
-   * @dev Reads a bytes32 value from a position in a byte array.
-   * @param b Byte array containing a bytes32 value.
-   * @param index Index in byte array of bytes32 value.
-   * @return result bytes32 value from byte array.
-   */
-  function readBytes32(
-    bytes memory b,
-    uint256 index
-  )
-    internal
-    pure
-    returns (bytes32 result)
-  {
-    require(
-      b.length >= index + 32,
-      "LibBytes#readBytes32: GREATER_OR_EQUAL_TO_32_LENGTH_REQUIRED"
-    );
-
-    // Arrays are prefixed by a 256 bit length parameter
-    index += 32;
-
-    // Read the bytes32 from array memory
-    assembly {
-      result := mload(add(b, index))
-    }
-    return result;
-  }
-}
-
 
 library SignatureValidator {
 	using LibBytes for bytes;
