220d18887ae441b900ca4a751c2e9ea4625740e0
Ivo Georgiev <ivo@adex.network>
2021-09-06 23:58:27 +0300

    WIP: new proxy generator done

--- js/IdentityProxyDeploy2.js
+++ js/IdentityProxyDeploy2.js
@@ -1,3 +1,5 @@
+// @TODO: use ethers v5
+// @TODO test: if something matches a particular bytecode
 const abi = require('ethereumjs-abi')
 const keccak256 = require('js-sha3').keccak256
 
@@ -10,10 +12,34 @@ function evmPush(data) {
 	return Buffer.concat([opCodeBuf, data])
 }
 
-function sstoreCode(slotNumber, keyType, key, valueType, value) {
+function sstoreCode(slotNumber, keyType, key, valueType, valueBuf) {
 	const buf = abi.rawEncode([keyType, valueType], [key, slotNumber])
 	const slot = keccak256(buf)
+	return Buffer.concat([
+		evmPush(valueBuf),
+		evmPush(Buffer.from(slot, 'hex')),
+		Buffer.from('55', 'hex')
+	])
 }
 
-module.exports = { evmPush }
+function getProxyDeployBytecode(masterContractAddr, privLevels, opts = { privSlot: 0 }) {
+	const { privSlot = 0 } = opts
+	const storage = Buffer.concat(privLevels
+		.map(([addr, data]) => {
+			return data ?
+				sstoreCode(privSlot, 'address', addr, 'bytes32', data)
+				: sstoreCode(privSlot, 'address', addr, 'bool', Buffer.from('01', 'hex'))
+		})
+	)
+	const initial = Buffer.from('3d602d80', 'hex')
+	const offset = storage.length + initial.length + 6 // 6 more bytes including the push added later on
+	return Buffer.concat([
+		storage,
+		initial,
+		evmPush(Buffer.from(offset.toString(16), 'hex')),
+		Buffer.from(`3d3981f3363d3d373d3d3d363d73${masterContractAddr.slice(2)}5af43d82803e903d91602b57fd5bf3`, 'hex')
+	])
+}
+
+module.exports = { evmPush, sstoreCode, getProxyDeployBytecode }
 
