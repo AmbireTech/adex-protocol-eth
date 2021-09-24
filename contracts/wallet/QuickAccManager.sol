1d42f616298a290cec60d72f128c87cbd608ee08
Ivo Georgiev <ivo@strem.io>
2021-09-24 19:04:43 +0300

    QuickAccManager abi added

--- contracts/wallet/QuickAccManager.sol
+++ contracts/wallet/QuickAccManager.sol
@@ -5,6 +5,8 @@ import "../Identity.sol";
 import "../interfaces/IERC20.sol";
 
 contract QuickAccManager {
+	// Note: nonces are scoped by identity rather than by accHash - the reason for this is that there's no reason to scope them by accHash,
+	// we merely need them for replay protection
 	mapping (address => uint) nonces;
 	mapping (bytes32 => uint) scheduled;
 
