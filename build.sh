729f36cf4b801a2f57bd90bc619acbdd885f30ab
Ivo Georgiev <ivo@adex.network>
2021-09-24 19:04:43 +0300

    QuickAccManager abi added

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ solc --overwrite --optimize --abi --bin contracts/OUTPACE.sol -o temp
 solc --overwrite --optimize --allow-paths . --abi --bin contracts/deposits/Depositor.sol -o temp
 solc --overwrite --optimize --allow-paths . --abi --bin contracts/deposits/Sweeper.sol -o temp
 solc --overwrite --abi contracts/Identity.sol -o temp 
+solc --overwrite --abi --allow-paths . contracts/wallet/QuickAccManager.sol -o temp
 solc --overwrite --abi contracts/IdentityFactory.sol -o temp 
 solc --overwrite --abi contracts/StakingPool.sol -o temp 
 
@@ -24,6 +25,7 @@ mv temp/OUTPACE.abi abi/OUTPACE.json
 mv temp/Identity.abi abi/Identity5.2.json  
 mv temp/IdentityFactory.abi abi/IdentityFactory5.2.json
 mv temp/StakingPool.abi abi/StakingPool.json
+mv temp/QuickAccManager.abi abi/QuickAccManager.json
 
 # Sweeper abi & bin file
 mv temp/Sweeper.abi abi/Sweeper.json
