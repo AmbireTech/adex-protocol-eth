a376341f7bbb81692976cb524a15e58aff4f6529
Ivo Georgiev <ivo@strem.io>
2021-10-02 18:37:53 +0300

    JS cleanup

--- js/index.js
+++ js/index.js
@@ -2,14 +2,12 @@ const channelLib = require('./Channel')
 const withdrawalLib = require('./Withdraw')
 const unbondCommitmentlLib = require('./UnbondCommitment')
 
-const identityLib = require('./Identity')
 const MerkleTree = require('./MerkleTree')
 const splitSig = require('./splitSig')
 const Permit = require('./Permit')
 
 module.exports = {
 	...channelLib,
-	...identityLib,
 	...withdrawalLib,
 	...unbondCommitmentlLib,
 	...Permit,
