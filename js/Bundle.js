077591eec77fc15443c392a74d4438ae2cc17915
Ivo Georgiev <ivo@adex.network>
2021-10-04 16:42:42 +0300

    Bundle API is ready

--- js/Bundle.js
+++ js/Bundle.js
@@ -9,25 +9,49 @@ const QuickAccManagerInterface = require('../abi/QuickAccManager')
 
 function Bundle(args) {
 	this.identity = ensure.Address(args.identity)
+	this.network = args.network
 	// @TODO validate this
 	this.signer = args.signer
 	this.txns = args.txns
 	return this
 }
 
-function hashTxns(identityAddr, chainId, nonce, txns) {
+Bundle.prototype.getNonce = async function(provider) {
+	this.nonce = await getNonce(provider, this)
+	return this.nonce
+}
+
+Bundle.prototype.estimate = async function({ fetch, relayerURL }) {
+	const res = await fetchPost(fetch, `${relayerURL}/identity/${this.identity}/${this.network}/estimate`, { txns: this.txns, signer: this.signer })
+	this.gasLimit = res.gasLimit
+	return res
+}
+
+Bundle.prototype.sign = async function(wallet) {
+	// @TODO quickAccount
+	const signature = await signMsg(wallet, hashTxns(this.identity, this.network, this.nonce, this.txns))
+	this.signature = signature
+	return signature
+}
+
+Bundle.prototype.submit = async function({ fetch, relayerURL }) {
+	const res = await fetchPost(fetch, `${relayerURL}/identity/${this.identity}/${this.network}/submit`, this)
+
+}
+
+function hashTxns (identityAddr, chainId, nonce, txns) {
 	const abiCoder = new AbiCoder()
 	const encoded = abiCoder.encode(['address', 'uint', 'uint', 'tuple(address, uint, bytes)[]'], [identityAddr, chainId, nonce, txns])
 	return arrayify(keccak256(encoded))
 }
 
-function mapSignatureV(sig) {
+function mapSignatureV (sig) {
 	sig = arrayify(sig)
 	if (sig[64] < 27) sig[64] += 27
 	return hexlify(sig)
 }
 
-async function signMsg(wallet, hash) {
+async function signMsg (wallet, hash) {
 	//assert.equal(hash.length, 32, 'hash must be 32byte array buffer')
 	// 02 is the enum number of EthSign signature type
 	return mapSignatureV(await wallet.signMessage(hash)) + '02'
@@ -45,6 +69,16 @@ async function getNonce (provider, userTxnBundle) {
 		else throw e
 	}
 }
+
+async function fetchPost (fetch, url, body) {
+	const r = await fetch(url, {
+		headers: { 'content-type': 'application/json' },
+		method: 'POST',
+		body: JSON.stringify(body)
+	})
+	return r.json()
+}
+
 //getNonce(require('ethers').getDefaultProvider('homestead'), { identity: '0x23c2c34f38ce66ccc10e71e9bb2a06532d52c5e8', signer: {address: '0x942f9CE5D9a33a82F88D233AEb3292E680230348'}, txns: [] }).then(console.log)
 
 module.exports = { Bundle }
