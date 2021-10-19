0580f13b96202f6570f8b56db7df197303511e19
Ivo Georgiev <ivo@strem.io>
2021-10-19 21:49:58 +0300

    fixed pragma instead of floating

--- contracts/extra/ADXLoyaltyArbitrage.sol
+++ contracts/extra/ADXLoyaltyArbitrage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
 
 import "../interfaces/IERC20.sol";
 
--- contracts/mocks/MockChainlink.sol
+++ contracts/mocks/MockChainlink.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
 
 contract MockChainlink {
     function latestAnswer() external pure returns (uint256) {
