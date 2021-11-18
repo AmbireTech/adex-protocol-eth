ded3621486ad81b7b30325a4b1f11cffac3978a8
Ivo Georgiev <ivo@strem.io>
2021-11-19 01:16:45 +0200

    RemainingBalancesOracle: also return gasleft

--- contracts/wallet/RemainingBalancesOracle.sol
+++ contracts/wallet/RemainingBalancesOracle.sol
@@ -21,4 +21,21 @@ contract RemainingBalancesOracle {
 		}
 		return results;
 	}
+
+	function getRemainingBalancesAndGas(
+		IdentityFactory factory,
+		bytes calldata code, uint256 salt,
+		Identity.Transaction[] calldata txns, bytes calldata signature,
+		address identity,
+		address[] calldata tokenAddrs
+	) external returns (uint[] memory, uint) {
+		factory.deployAndExecute(code, salt, txns, signature);
+		uint len = tokenAddrs.length;
+		uint[] memory results = new uint[](len);
+		for (uint256 i = 0; i < len; i++) {
+			if (tokenAddrs[i] == address(0)) results[i] = address(identity).balance;
+			else results[i] = IERC20(tokenAddrs[i]).balanceOf(identity);
+		}
+		return (results, gasleft());
+	}
 }
