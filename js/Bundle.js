aaa36aa086351a99884ff024c341d2a42b9dbd5e
Ivo Georgiev <ivo@adex.network>
2021-11-18 13:14:55 +0200

    js/Bundle: expose signMsg

--- js/Bundle.js
+++ js/Bundle.js
@@ -224,4 +224,4 @@ async function estimateGasWithCatch (provider, blockTag, tx) {
 
 // getNonce(require('ethers').getDefaultProvider('homestead'), { identity: '0x23c2c34f38ce66ccc10e71e9bb2a06532d52c5e8', signer: {address: '0x942f9CE5D9a33a82F88D233AEb3292E680230348'}, txns: [] }).then(console.log)
 
-module.exports = { Bundle, signMsgHash, getSignable }
+module.exports = { Bundle, signMsgHash, getSignable, signMsg }
