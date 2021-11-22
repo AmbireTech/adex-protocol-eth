28ba30c1e1f134fdf12ded959f667bc096606652
Lachezar Lechev <lachezar@ambire.com>
2021-11-22 14:44:21 +0200

    build - Identity5.2 bin

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ mkdir -p temp
 solc --overwrite --optimize --abi --bin contracts/OUTPACE.sol -o temp
 solc --overwrite --optimize --allow-paths . --abi --bin contracts/deposits/Depositor.sol -o temp
 solc --overwrite --optimize --allow-paths . --abi --bin contracts/deposits/Sweeper.sol -o temp
-solc --overwrite --abi contracts/Identity.sol -o temp 
+solc --overwrite --abi --bin contracts/Identity.sol -o temp
 solc --overwrite --abi --allow-paths . contracts/wallet/QuickAccManager.sol -o temp
 solc --overwrite --abi --allow-paths . contracts/wallet/RemainingBalancesOracle.sol -o temp
 solc --overwrite --abi contracts/IdentityFactory.sol -o temp 
@@ -23,11 +23,18 @@ mkdir -p resources abi
 
 # ABIs
 mv temp/OUTPACE.abi abi/OUTPACE.json
-mv temp/Identity.abi abi/Identity5.2.json  
+mv temp/Identity.abi abi/Identity5.2.json
 mv temp/IdentityFactory.abi abi/IdentityFactory5.2.json
 mv temp/StakingPool.abi abi/StakingPool.json
 mv temp/QuickAccManager.abi abi/QuickAccManager.json
 mv temp/RemainingBalancesOracle.abi abi/RemainingBalancesOracle.json
+
+# Identity bin file
+#### we require this file for the Rust implementation
+cp temp/Identity.bin resources/bytecode/Identity5.2.bin
+# copy the Identity.bin to Identity5.2.bin because `change_bytecode_to_json_and_move`
+cp temp/Identity.bin temp/Identity5.2.bin
+change_bytecode_to_json_and_move "Identity5.2"
 
 # Sweeper abi & bin file
 mv temp/Sweeper.abi abi/Sweeper.json
