c254b26e70aaee9f929c5bc484ff1885e4e67371
Ivo Georgiev <ivo@ambire.com>
2026-01-03 17:53:28 +0200

    StakingMigrator: do not give away residual amount if any

--- contracts/StakingPoolMigratorGovernance.sol
+++ contracts/StakingPoolMigratorGovernance.sol
@@ -16,6 +16,9 @@ contract StakingMigratorGovernance {
 	function migrate(IStakingPool oldPool, uint shares, bool skipMint, IStakingPool newPool) external {
 		require(block.timestamp <= deadline, "migrating past deadline");
 
+		IERC20 token = IERC20(oldPool.baseToken());
+		uint256 startAmount = token.balanceOf(address(this));
+
 		// first, pull the old staking tokens and temporarily set rage leave percentage so that we can pull the baseToken
 		IERC20(address(oldPool)).transferFrom(msg.sender, address(this), shares); 
 		uint rageReceived = oldPool.rageReceivedPromilles();
@@ -24,10 +27,8 @@ contract StakingMigratorGovernance {
 		oldPool.setRageReceived(rageReceived);
 
 		// then stake all baseTokens we have in the new pool
-		// if we happen to get extra tokens, user gets them
-		IERC20 token = IERC20(oldPool.baseToken());
 		require(token == IERC20(newPool.baseToken()), "baseToken not the same");
-		uint tokenAmount = token.balanceOf(address(this));
+		uint tokenAmount = token.balanceOf(address(this)) - startAmount;
 		token.approve(address(newPool), tokenAmount);
 		newPool.enterTo(msg.sender, tokenAmount);
 	}
